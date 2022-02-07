a9d7e0cd4aa3eab7c41e2560627d7d17b4fa53ae
Austin C <austin.cagney@automsoft.com>
2022-02-07 11:50:53 +0000

    Update to latest .NET Framework and compiler version

--- REST API/CSharp/DataReadExample/ReadDataREST.cs
+++ REST API/CSharp/DataReadExample/ReadDataREST.cs
@@ -1,83 +1,22 @@
-﻿using Models;
-using Models.Backend.DTO.V1_1;
-using Models.DTO.V1;
-using ServiceStack;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
+using ServiceStack;
+using Models.Backend.DTO.V1_2;
+using Models.DTO.V1;
 
 namespace ReadDataREST
 {
-    internal class Program
+    class SampleClient
     {
-
         public static void Main(string[] args)
         {
             try
             {
-                // Connect to REST API Server.
-                ServiceClientBase _client = Connect();
+                var _client = new SampleClient();
 
-                // Get list of RAPID databases on system.
-                List<string> databases = GetDatabases(_client);
-
-                // Iterate through databases.
-                foreach (string db in databases)
-                {
-                    // Get alarm nodes in database.
-                    List<string> alarmNodes = GetAlarmNodes(_client, db);
-
-                    // Iterate through alarm nodes.
-                    foreach (string alarmNode in alarmNodes)
-                    {
-                        // Get alarm areas in alarm node.
-                        List<string> alarmAreas = GetAlarmAreas(_client, db, alarmNode);
-
-                        // Get alarm data for alarm areas in node.
-                        AlarmDataResponse alarmData = GetAlarmData(_client, db, alarmNode, alarmAreas);
-
-                        // Iterate through alarm data points and output to console.
-                        foreach (AlarmDataPoint point in alarmData.AlarmData)
-                        {
-                            // Build Vendor Defined attribute summary string.
-                            string vdaSummary = "";
-                            for (int i=0;i<point.VendorDefinedAttributeIDs.Count;i++)
-                            {
-                                vdaSummary += point.VendorDefinedAttributeIDs[i] + ":" + point.VendorDefinedAttributeValues[i];
-                                if (i < point.VendorDefinedAttributeIDs.Count - 1)
-                                    vdaSummary += ";";
-                            }
-
-                            // Output alarm data point to console.
-                            Console.WriteLine("Alarm time: " + point.EventTime + ",Source: " + point.Source + ",Message: " + point.Message + ",Severity: " + point.Severity + ",Attribute data: " + vdaSummary);
-                        }
-                    }
-
-                    // Get process nodes in database.
-                    List<string> processNodes = GetProcessNodes(_client, db);
-
-                    // Iterate through process nodes.
-                    foreach (string processNode in processNodes)
-                    {
-                        // Get alarm areas in alarm node.
-                        List<string> itemNames = GetItems(_client, db, processNode);
-
-                        foreach (string itemName in itemNames)
-                        {
-                            // Get process data for node.
-                            ProcessDataResponse procesData = GetProcessData(_client, db, processNode, itemName);
-
-                            // Iterate through process data points and output to console.
-                            foreach (ProcessDataPoint point in procesData.ProcessData)
-                            {
-                                // Output alarm data point to console.
-                                Console.WriteLine("Time: " + point.EventTime + ",Value: " + point.EventValue + ",Quality: " + point.Quality.ToString());
-                            }
-                        }
-                    }
-
-                }
+                _client.Connect();
+                _client.QueryData();
             }
             catch (Exception ex)
             {
@@ -86,14 +25,88 @@ namespace ReadDataREST
             }
         }
 
-        /// <summary>
-        /// Get databases on system.
-        /// </summary>
-        /// <param name="client"></param>
-        /// <returns></returns>
-        public static List<string> GetDatabases(ServiceClientBase client)
+        public void QueryData()
         {
-            return new List<string>(client.Get(new DatabaseList()) as string[]);
+            // Get list of RAPID databases on system.
+            var databases = GetDatabases();
+
+            // Iterate through databases.
+            foreach (var db in databases)
+            {
+                // query process data for database
+                QueryProcessData(db);
+
+                // query alarm data for database
+                QueryAlarmData(db);
+            }
+        }
+
+        void QueryProcessData(string db)
+        {
+            // Get process nodes in database.
+            var processNodes = GetProcessNodes(db);
+
+            // Iterate through process nodes.
+            foreach (var processNode in processNodes)
+            {
+                // Get alarm areas in alarm node.
+                var itemNames = GetItems(db, processNode);
+
+                foreach (var itemName in itemNames)
+                {
+                    // Get process data for node.
+                    var procesData = GetProcessData(db, processNode, itemName);
+
+                    // Iterate through process data points and output to console.
+                    foreach (var point in procesData.ProcessData)
+                    {
+                        // Output alarm data point to console.
+                        Console.WriteLine("Time: " + point.EventTime + ",Value: " + point.EventValue + ",Quality: " + point.Quality.ToString());
+                    }
+                }
+            }
+        }
+
+        void QueryAlarmData(string db)
+        {
+            // Get alarm nodes in database.
+            var alarmNodes = GetAlarmNodes(db);
+
+            // Iterate through alarm nodes.
+            foreach (var alarmNode in alarmNodes)
+            {
+                // Get alarm areas in alarm node.
+                var alarmAreas = GetAlarmAreas(db, alarmNode);
+
+                // Get alarm data for alarm areas in node.
+                var alarmData = GetAlarmData(db, alarmNode, alarmAreas);
+
+                // Iterate through alarm data points and output to console.
+                foreach (var point in alarmData.AlarmData)
+                {
+                    // Build Vendor Defined attribute summary string.
+                    var vdaSummary = "";
+                    for (var i = 0; i < point.VendorDefinedAttributeIDs.Count; i++)
+                    {
+                        vdaSummary += point.VendorDefinedAttributeIDs[i] + ":" + point.VendorDefinedAttributeValues[i];
+                        if (i < point.VendorDefinedAttributeIDs.Count - 1)
+                            vdaSummary += ";";
+                    }
+
+                    // Output alarm data point to console.
+                    Console.WriteLine("Alarm time: " + point.EventTime + ",Source: " + point.Source + ",Message: " + point.Message + ",Severity: " + point.Severity + ",Attribute data: " + vdaSummary);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Get databases from REST Server.
+        /// </summary>
+        /// <param name="_client"></param>
+        /// <returns></returns>
+        List<string> GetDatabases()
+        {
+            return new List<string>(_client.Get(new DatabaseList()) as string[]);
         }
 
    
@@ -101,61 +114,62 @@ namespace ReadDataREST
         /// <summary>
         /// Get alarm nodes from database.
         /// </summary>
-        /// <param name="client"></param>
+        /// <param name="_client"></param>
         /// <param name="dbName"></param>
         /// <returns></returns>
-        public static List<string> GetAlarmNodes(ServiceClientBase client, string dbName)
+        List<string> GetAlarmNodes(string dbName)
         {
-            string rawDBName = dbName.Substring(dbName.LastIndexOf("/") + 1);
-            return new List<string>(client.Get(new NodeList() { DbName = rawDBName, Filter = "*", Type = "OPC_AE" }) as string[]);
+            var rawDBName = dbName.Substring(dbName.LastIndexOf("/") + 1);
+            return new List<string>(_client.Get(new NodeList() { DbName = rawDBName, Filter = "*", Type = "OPC_AE" }) as string[]);
         }
 
         /// <summary>
         /// Get alarm areas from database, node.
         /// </summary>
-        /// <param name="client"></param>
+        /// <param name="_client"></param>
         /// <param name="dbName"></param>
         /// <param name="nodeName"></param>
         /// <returns></returns>
-        public static List<string> GetAlarmAreas(ServiceClientBase client, string dbName, string nodeName)
+        public List<string> GetAlarmAreas(string dbName, string nodeName)
         {
-            string rawDBName = dbName.Substring(dbName.LastIndexOf("/") + 1);
-            string rawAlarmNodeName = nodeName.Substring(nodeName.LastIndexOf("/") + 1);
-            return client.Get(new AlarmAreaList() { DbName = rawDBName, NodeName = rawAlarmNodeName });
+            var rawDBName = dbName.Substring(dbName.LastIndexOf("/") + 1);
+            var rawAlarmNodeName = nodeName.Substring(nodeName.LastIndexOf("/") + 1);
+            return _client.Get(new AlarmAreaList() { DbName = rawDBName, NodeName = rawAlarmNodeName });
         }
 
 
         /// <summary>
         /// Get Alarm data for alarm areas (for last day)
         /// </summary>
-        /// <param name="client"></param>
+        /// <param name="_client"></param>
         /// <param name="dbName"></param>
         /// <param name="nodeName"></param>
         /// <param name="alarmAreas"></param>
         /// <returns></returns>
-        public static AlarmDataResponse GetAlarmData(ServiceClientBase client, string dbName, string nodeName, List<string> alarmAreas)
+        AlarmDataResponseV1_2 GetAlarmData(string dbName, string nodeName, List<string> alarmAreas)
         {
-            string rawDBName = dbName.Substring(dbName.LastIndexOf("/") + 1);
-            string rawAlarmNodeName = nodeName.Substring(nodeName.LastIndexOf("/") + 1);
+            var rawDBName = dbName.Substring(dbName.LastIndexOf("/") + 1);
+            var rawAlarmNodeName = nodeName.Substring(nodeName.LastIndexOf("/") + 1);
 
-            List<string> rawAlarmAreas = new List<string>();
-            for (int i = 0; i < alarmAreas.Count; i++)
+            var rawAlarmAreas = new List<string>();
+            for (var i = 0; i < alarmAreas.Count; i++)
                 rawAlarmAreas.Add(alarmAreas[i].Substring(alarmAreas[i].LastIndexOf("/") + 1));
 
-            AlarmDataRead alarmDataRead = new AlarmDataRead();
+            var alarmDataRead = new AlarmDataReadV1_2();
             alarmDataRead.DbName = rawDBName;
             alarmDataRead.NodeName = rawAlarmNodeName;
             alarmDataRead.AlarmAreas = rawAlarmAreas;
+
+            // retrieve alarms from last 30 days
             alarmDataRead.StartTime = DateTime.Now.Subtract(TimeSpan.FromDays(30)).ToString();
             alarmDataRead.EndTime = DateTime.Now.ToString();
             alarmDataRead.FieldFilterIDs = new List<AlarmFieldFilter_e>() { AlarmFieldFilter_e.Source };
 
-            List<nuaViews.Model.ExternalServices.Variant> sourceFilter = new List<nuaViews.Model.ExternalServices.Variant>() { new nuaViews.Model.ExternalServices.Variant("*") };
-            alarmDataRead.FieldFilterValues = new List<List<nuaViews.Model.ExternalServices.Variant>>() { sourceFilter };
+            alarmDataRead.FieldFilterValues = new List<string>() { "*" };
             alarmDataRead.ReturnedFields = new List<AlarmFieldFilter_e>() { AlarmFieldFilter_e.Source, AlarmFieldFilter_e.EventType, AlarmFieldFilter_e.VendorAttributeID, AlarmFieldFilter_e.Message, AlarmFieldFilter_e.Severity };
 
             // Return alarm data.
-            return client.Put(alarmDataRead);
+            return _client.Put(alarmDataRead);
         }
 
         #endregion
@@ -165,84 +179,85 @@ namespace ReadDataREST
         /// <summary>
         /// Get process nodes from database.
         /// </summary>
-        /// <param name="client"></param>
+        /// <param name="_client"></param>
         /// <param name="dbName"></param>
         /// <returns></returns>
-        public static List<string> GetProcessNodes(ServiceClientBase client, string dbName)
+        List<string> GetProcessNodes(string dbName)
         {
-            string rawDBName = dbName.Substring(dbName.LastIndexOf("/") + 1);
-            return new List<string>(client.Get(new NodeList() { DbName = rawDBName, Filter = "*", Type= "OPC,DataEntry" }) as string[]);
+            var rawDBName = dbName.Substring(dbName.LastIndexOf("/") + 1);
+            return new List<string>(_client.Get(new NodeList() { DbName = rawDBName, Filter = "*", Type= "OPC,DataEntry" }) as string[]);
         }
 
 
         /// <summary>
         /// Get alarm areas from database, node.
         /// </summary>
-        /// <param name="client"></param>
+        /// <param name="_client"></param>
         /// <param name="dbName"></param>
         /// <param name="nodeName"></param>
         /// <returns></returns>
-        public static List<string> GetItems(ServiceClientBase client, string dbName, string nodeName)
+        List<string> GetItems(string dbName, string nodeName)
         {
-            string rawDBName = dbName.Substring(dbName.LastIndexOf("/") + 1);
-            string rawNodeName = nodeName.Substring(nodeName.LastIndexOf("/") + 1);
-            return new List<string>(client.Get(new ItemList() { DbName = rawDBName, NodeName = rawNodeName }));
+            var rawDBName = dbName.Substring(dbName.LastIndexOf("/") + 1);
+            var rawNodeName = nodeName.Substring(nodeName.LastIndexOf("/") + 1);
+            return new List<string>(_client.Get(new ItemList() { DbName = rawDBName, NodeName = rawNodeName }));
         }
 
 
         /// <summary>
         /// Get process data for process node.
         /// </summary>
-        /// <param name="client"></param>
+        /// <param name="_client"></param>
         /// <param name="dbName"></param>
         /// <param name="nodeName"></param>
         /// <returns></returns>
-        public static ProcessDataResponse GetProcessData(ServiceClientBase client, string dbName, string nodeName, string itemName)
+        ProcessDataResponseV1_2 GetProcessData(string dbName, string nodeName, string itemName)
         {
-            string rawDBName = dbName.Substring(dbName.LastIndexOf("/") + 1);
-            string rawNodeName = nodeName.Substring(nodeName.LastIndexOf("/") + 1);
-            string rawItemName = itemName.Substring(itemName.LastIndexOf("/") + 1);
+            var rawDBName = dbName.Substring(dbName.LastIndexOf("/") + 1);
+            var rawNodeName = nodeName.Substring(nodeName.LastIndexOf("/") + 1);
+            var rawItemName = itemName.Substring(itemName.LastIndexOf("/") + 1);
 
-            ProcessDataReadV1_1 processDataRead = new ProcessDataReadV1_1();
+            var processDataRead = new ProcessDataReadV1_2();
             processDataRead.DbName = rawDBName;
             processDataRead.NodeName = rawNodeName;
             processDataRead.ItemName = rawItemName;
+
+            // retrieve 100 data points from last 30 days
             processDataRead.StartTime = DateTime.Now.Subtract(TimeSpan.FromDays(30));
             processDataRead.EndTime = DateTime.Now;
             processDataRead.Aggregate = ProcessDataRequest.Aggregate_e.Maximum;
             processDataRead.ResampleCount = 100;
-            
 
             // Return alarm data.
-            return client.Put(processDataRead);
+            return _client.Put(processDataRead);
         }
+        #endregion
 
-   /// <summary>
+        ServiceClientBase _client;
+
+        /// <summary>
         /// Connect to REST Server. ** Note: username and password should be changed according to REST Server authentication.
         /// </summary>
         /// <param name="serverURL"></param>
         /// <param name="username"></param>
         /// <param name="password"></param>
         /// <returns></returns>
-        public static ServiceClientBase Connect(string serverURL = "http://localhost:3001", string username = "admin", string password = "admin")
+        public void Connect(string serverURL = "http://localhost:3001", string username = "admin", string password = "admin")
         {
             //TODO: Check lifetime on connection. If it has expired, make a new connection
-            JsonServiceClient client = new JsonServiceClient(serverURL);
+            _client = new JsonServiceClient(serverURL);
 
             if (!string.IsNullOrEmpty(username))
             {
-                var response = client.Post(new Authenticate
+                var response = _client.Post(new Authenticate
                 {
                     provider = "AutomsoftAuthProvider",
                     UserName = username,
                     Password = password,
-                    RememberMe = true, //important tell client to retain permanent cookies
+                    RememberMe = true, //important tell _client to retain permanent cookies
                 });
 
             }
-            return client;
         }
-
-        #endregion
     }
 }
